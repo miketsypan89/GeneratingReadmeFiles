c33460522fd35ba4f462eede2ddce0aa73ef8924
Mike Tsypan <miket@victorytransport.com>
2023-04-17 18:33:08 -0400

    added markdown js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("yo!")
+// TODO: Include packages needed for this application
+const generateMarkdown = require("./markdown.js");
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "input", 
+        name: "title",
+        message: "Name your project",
+    },
+    {
+        
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) { }
+
+// TODO: Create a function to initialize app
+function init() { }
+
+// Function call to initialize app
+init();
+
+console.log(questions)
