a9e52ee8ff2c88f1abb9e502bf9c8c09285bce10
Mike Tsypan <miket@victorytransport.com>
2023-04-20 18:24:29 -0400

    added few initial questions for the generator

--- index.js
+++ index.js
@@ -3,25 +3,49 @@ const generateMarkdown = require("./markdown.js");
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-// TODO: Create an array of questions for user input
+// An array of questions for user input
 const questions = [
     {
-        type: "input", 
+        type: "input",
         name: "title",
-        message: "Name your project",
+        message: "What is the name of your Project?",
     },
     {
-        
-    }
+        type: "input",
+        name: "description",
+        message: "Describe your project:",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+    },
+
+
 ];
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            console.log(answers)
+            console.log(generateMarkdown(answers))
+            generateMarkdown(answers)
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+                // Prompt couldn't be rendered in the current environment
+            } else {
+                // Something else went wrong
+            }
+        });
+}
 
 // Function call to initialize app
 init();
-
-console.log(questions)
--- markdown.js
+++ markdown.js
@@ -1,6 +1,20 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) { }
+// A function that returns a license badge based on which license is passed.
+// If there is no license, an empty string will be returned.
+function renderLicenseBadge(license) {
+    switch (licenseName.toLowerCase()) {
+        case 'mit':
+            return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+        case 'apache':
+            return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+        case 'gnu gplv3':
+            return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+        case 'unlicense':
+            return '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)';
+        default:
+            return '';
+            console.log(renderLicenseBadge);
+    }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,9 +26,9 @@ function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-    return `# ${data.title}
+    return ` # ${answers.title} # ${answers.description} # ${answers.email}`;
 
-`;
+
 }
 
 module.exports = generateMarkdown;
